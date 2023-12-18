a9d946a430e33e055f88d1534f81191ade57b48e
Admin <Admin@DESKTOP-IS6L354>
2023-12-18 20:56:32 +0530

    Depth First search - Find All Paths for a Sum

---
+++ datastructurespatterns/src/main/java/com/ds/patterns/depthFirstSearch/AllPathsForSum.java
@@ -0,0 +1,52 @@
+package com.ds.patterns.depthFirstSearch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AllPathsForSum {
+	
+	private static void findAllPaths(TreeNode root, int sum, List<Integer> intermediateList, List<List<Integer>> finalList) {
+		
+		if(root == null) {
+			return;
+		}
+			
+		intermediateList.add(root.value);
+		
+		if(root.left == null && root.right == null && root.value == sum) {
+			finalList.add(new ArrayList<Integer>(intermediateList));
+			
+		}else {
+			findAllPaths(root.left, sum - root.value, intermediateList, finalList);
+			
+			findAllPaths(root.right, sum - root.value, intermediateList, finalList);
+		}
+			
+		System.out.println("root.value "+root.value+ " intermediateList -- "+intermediateList.toString());
+		intermediateList.remove(intermediateList.size() - 1);		
+	}
+	
+	private static List<List<Integer>> fetchAllTreePaths(TreeNode root, int sum) {
+		
+		List<List<Integer>> finalList = new ArrayList<>();
+		List<Integer> intermediateList = new ArrayList<>();
+		
+		findAllPaths(root, sum, intermediateList, finalList);
+		
+		return finalList;
+	}
+	
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(12);
+		root.left = new TreeNode(7);
+		root.right = new TreeNode(1);
+		root.left.left = new TreeNode(4);
+		root.right.left = new TreeNode(10);
+		root.right.right = new TreeNode(5);
+		
+		List<List<Integer>> finalList = fetchAllTreePaths(root, 23);
+		System.out.println(finalList.toString());
+	}
+
+}
